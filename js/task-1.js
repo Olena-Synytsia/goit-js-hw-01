3dbdb6c0bdd9b86fbd32eeeb0ca8b274c9ec9931
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-05-16 13:59:32 +0300

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  const makeTransaction = `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
   return makeTransaction;
 }
 
